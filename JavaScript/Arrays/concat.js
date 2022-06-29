b809375289f2fa3891cd12cd7718c597e8c1b8f1
Miller Vasco <millermartinsvasco1@gmail.com>
2022-06-29 07:42:16 -0300

    0742

--- JavaScript/Arrays/concat.js
+++ JavaScript/Arrays/concat.js
@@ -2,3 +2,5 @@ const filhas = ['Valesca', 'Cibalena']
 const filhos = ['Voxiton', 'Vesclei']
 const todos = filhas.concat(filhos)
 console.log(todos, filhas, filhos)
+
+console.log([].concat([1,2],[3,4],[[6,7]]))
